b7f08031c132e1c6294566da31cee0dcb247f5fa
Pedro Silva <pedro_rike@hotmail.com.br>
2023-10-25 21:38:33 -0300

    feat: added footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-accent px-8 py-4 text-sm opacity-75">
+      © 2023 Copyright <span className="font-semibold">FSW</span> Store
+    </div>
+  );
+};
+
+export default Footer;
