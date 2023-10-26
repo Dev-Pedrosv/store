a488294ebb4bd0ff1a0e368c7e0a0b9b6e8dc92f
Pedro Silva <pedro_rike@hotmail.com.br>
2023-10-25 22:17:04 -0300

    feat: category page

---
+++ src/app/category/[slug]/page.tsx
@@ -0,0 +1,43 @@
+import ProductItem from "@/components/product-item";
+import { Badge } from "@/components/ui/badge";
+import { CATEGORY_ICON } from "@/constants/category-icon";
+import { computeProductsTotalPrice } from "@/helpers/products";
+import { prismaClient } from "@/lib/prisma";
+
+const CategoryProducts = async ({ params }: any) => {
+  const category = await prismaClient.category.findFirst({
+    where: {
+      slug: params.slug,
+    },
+    include: {
+      Product: true,
+    },
+  });
+
+  if (!category) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col gap-8 p-5">
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-3 py-1.5 text-base uppercase"
+        variant="outline"
+      >
+        {CATEGORY_ICON[params.slug as keyof typeof CATEGORY_ICON]}
+        {category.name}
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-8">
+        {category.Product.map((product) => (
+          <ProductItem
+            product={computeProductsTotalPrice(product)}
+            key={product.id}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryProducts;
