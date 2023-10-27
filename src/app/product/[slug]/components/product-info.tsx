18ee56110e8cf1e7bb29196731d2b49cef66c0ac
Pedro Silva <pedro_rike@hotmail.com.br>
2023-10-26 22:03:49 -0300

    feat: add discount badge components

--- src/app/product/[slug]/components/product-info.tsx
+++ src/app/product/[slug]/components/product-info.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { Badge } from "@/components/ui/badge";
+import DiscountBadge from "@/components/discount-badge";
 import { Button } from "@/components/ui/button";
 import { ProductWithTotalPrice } from "@/helpers/products";
-import { ArrowDownIcon, ArrowLeft, ArrowRight, TruckIcon } from "lucide-react";
+import { ArrowLeft, ArrowRight, TruckIcon } from "lucide-react";
 import { useState } from "react";
 
 interface ProductInfoProps {
@@ -33,10 +33,7 @@ const ProductInfo = ({
         <div className="flex items-center gap-1">
           <h1 className="text-xl font-bold">R$ {totalPrice.toFixed(2)}</h1>
           {discountPercentage > 0 && (
-            <Badge className="px-2 py-[2px]">
-              <ArrowDownIcon size={14} />
-              {discountPercentage} %
-            </Badge>
+            <DiscountBadge>{discountPercentage}</DiscountBadge>
           )}
         </div>
 
